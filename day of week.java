be1a269d53cb7d7007fafda5cf36bd6165577308
1315095 <priyankaboopathi.ece@gmail.com>
2016-08-22 18:21:53 +0530

    Add files via upload

---
+++ day of week.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+public holiday
+{
+public static void main(String[] arg);
+{
+Scanner in=new Scanner(System.in);
+String str;
+boolean return;
+str=("sunday","monday","tuesday","wednesday","thursday","friday",saturday");
+str=in.nextLine();
+if(str!=sunday)
+{
+reurn true;
+}
+else 
+return false;
+}
+}
